51cc928263c181d0ca8ec2617ae0c97172568aa8
keneilwemotona <keneilwem333@gmail.com>
2023-09-10 23:42:16 -0700

    hello_betty.c

---
+++ hello_betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 'write a beatiful code that passes Betty checks'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *message = "Hello, Betty!"
+
+		printf("%s\n", message);
+	return (0);
+}
