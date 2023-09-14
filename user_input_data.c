b2f932193669e21ba8df018d5e686535e0fbb43e
adelberd <adelberd@example.com>
2023-09-14 13:14:49 +0200

    Added a function to excute user commands

--- user_input_data.c
+++ user_input_data.c
@@ -8,5 +8,15 @@
 
 char *user_input_data()
 {
-  
+	char *data_input = NULL;
+	size_t buff_size = 0;
+
+	ssize_t characters = getline(&data_input, &buff_size, stdin);
+
+	if (characters == -1)
+	{
+		perror("Error, cannot read from stdin");
+		exit(EXIT_FAILURE);
+	}
+	return data_input;
 }
