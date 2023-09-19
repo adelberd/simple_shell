182f72a66e4db88c07d0a6b0841d882c0e4a135a
keneilwemotona <keneilwem333@gmail.com>
2023-09-19 00:11:59 -0700

    full_command_path.c

--- simple_shell.h
+++ simple_shell.h
@@ -15,5 +15,6 @@ char *user_input_data();
 int _strlen(char *s);
 void remove_newline_char(char *str);
 void execute_commands(char *user_input_data);
+void execute_command(char *command, char *path[])
 
 #endif /* SIMPLE_SHELL_H*/
